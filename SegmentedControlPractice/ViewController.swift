f94bb5f57362c88fc18254ac0265be40e09f22ad
Ron <onedarksky@icloud.com>
2021-10-15 13:33:58 +0800

    Initial Commit

---
+++ SegmentedControlPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControlPractice
+//
+//  Created by 江庸冊 on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
